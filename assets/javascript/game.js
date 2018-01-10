4ac04090ac9b7fe83eea59a364158d1b5e33ebf7
pawhalen <pgwhal@gmail.com>
2018-01-09 21:16:12 -0500

    many changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
         $("#status").html("");
 
         //Set the data properties that align with a character
-        $("#character1").data("attr", {name: "Obi-Wan Kenobi", hp: 120, ap: 8, bap: 8, cap: 10})
-        $("#character2").data("attr", {name: "Luke Skywalker", hp: 100, ap: 10, bap: 12, cap: 5})
-        $("#character3").data("attr", {name: "Darth Sidious", hp: 150, ap: 12, bap: 12, cap: 20})
-        $("#character4").data("attr", {name: "Darth Maul", hp: 180, ap: 14, bap: 14, cap: 25})
+        $("#character1").data("attr", {name: "Obi-Wan Kenobi", hp: 120, ap: 8, bap: 8, cap: 30})
+        $("#character2").data("attr", {name: "Luke Skywalker", hp: 100, ap: 20, bap: 20, cap: 5})
+        $("#character3").data("attr", {name: "Darth Sidious", hp: 150, ap: 10, bap: 10, cap: 20})
+        $("#character4").data("attr", {name: "Darth Maul", hp: 180, ap: 5, bap: 5, cap: 25})
 
         //Set the base healthpoint text
         $("#character1").children(".healthpoints").text($("#character1").data("attr").hp)
@@ -59,8 +59,6 @@ $(document).ready(function() {
             
         } else {
 
-            console.log("Try this - chosen :", chosenid, "enemy:", enemyid)
-            
             var chosenname = $(chosenid).data("attr").name;
             var enemyname = $(enemyid).data("attr").name;
             var chosenhp = $(chosenid).data("attr").hp;
@@ -71,18 +69,8 @@ $(document).ready(function() {
     
             $(enemyid).data("attr").hp = enemyhp-chosenap;
             $(chosenid).data("attr").ap = chosenap+chosenbap;
-
-            if ($(enemyid).data("attr").hp > 0) {
-
-
-                $("#status").html("<p>You attacked " + enemyname + " for " + chosenap + " damage.</p><p>" + enemyname + " attacked you for " + enemycap + " damage.</p>")
-        
-                $(chosenid).data("attr").hp = chosenhp-enemycap;
-
-                $(chosenid).children(".healthpoints").text($(chosenid).data("attr").hp);
-                $(enemyid).children(".healthpoints").text($(enemyid).data("attr").hp);
-
-            } else {
+            
+            if ($(enemyid).data("attr").hp <= 0) {
                 enemies.pop(enemyid);
 
                 if (enemies.length === 0) {
@@ -92,17 +80,27 @@ $(document).ready(function() {
 
                 } else {
 
-                $("#status").text("You have defeated " + enemyname + ", you can choose to fight another enemy.");
-                $(enemyid).hide();
-                enemyid = ""
-
+                    $("#status").text("You have defeated " + enemyname + ", you can choose to fight another enemy.");
+                    $(enemyid).hide();
+                    enemyid = ""
+                    return;
                 }
-            }
-            
+
+            } else {
+                $(chosenid).data("attr").hp = chosenhp-enemycap;
+                $(chosenid).children(".healthpoints").text($(chosenid).data("attr").hp);
+            } 
+
             if ($(chosenid).data("attr").hp <= 0) {
                 $("#status").text("You have been defeated! GAME OVER!!!");
                 return;
-            } 
+            
+            } else {
+                $("#status").html("<p>You attacked " + enemyname + " for " + chosenap + " damage.</p><p>" + enemyname + " attacked you for " + enemycap + " damage.</p>")
+                $(enemyid).children(".healthpoints").text($(enemyid).data("attr").hp);
+            }
+            
+           
         }
     });
     
@@ -114,7 +112,7 @@ $(document).ready(function() {
         $("#characters").append($("#character3"));
         $("#characters").append($("#character4"));
 
-        $("#characters").children().removeClass("enemy").addClass("character").show();
+        $("#characters").children().removeClass("enemy enemies").addClass("character").show();
 
     });
 
